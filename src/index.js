0fa164b91fda2fff18e4332cf76116c7e1bf185a
Souradeep Nanda <souradeep.nanda123@gmail.com>
2019-05-12 20:09:17 +0530

    Ignore file if unable to parse

--- src/index.js
+++ src/index.js
@@ -18,36 +18,41 @@ const inputDir = process.argv[2] || './'; // '../captive-app';
 const outputDir = process.argv[3] || './'; // '../captive-app';
 
 const transformFile = (fileName) => {
-  console.log('Transforming:', fileName);
-  const inputCode = fs.readFileSync(fileName, 'utf8');
+  try {
+    console.log('Transforming:', fileName);
+    const inputCode = fs.readFileSync(fileName, 'utf8');
 
-  const ast = babylon.parse(inputCode, {
-    sourceType: 'module',
-    plugins: ['jsx'],
-  });
+    const ast = babylon.parse(inputCode, {
+      sourceType: 'module',
+      plugins: ['jsx'],
+    });
 
-  traverse(ast, myPlugin(babel).visitor);
+    traverse(ast, myPlugin(babel).visitor);
 
-  // Convert all the ~/i18n/keys to <workplace_dir>/src/i18n/keys
-  const state = {
-    file: {
-      opts: {
-        sourceRoot: path.resolve(inputDir),
-        filename: fileName,
+    // Convert all the ~/i18n/keys to <workplace_dir>/src/i18n/keys
+    const state = {
+      file: {
+        opts: {
+          sourceRoot: path.resolve(inputDir),
+          filename: fileName,
+        },
       },
-    },
-    opts: {
-      sourceDir: 'src', // Default in React projects
-    },
-  };
-  traverse(ast, relativeImportPlugin(babel).visitor, null, state);
+      opts: {
+        sourceDir: 'src', // Default in React projects
+      },
+    };
+    traverse(ast, relativeImportPlugin(babel).visitor, null, state);
 
-  const { code } = babel.transformFromAst(ast);
+    const { code } = babel.transformFromAst(ast);
 
-  const relativePath = path.relative(inputDir, fileName);
-  const outputFilePath = path.join(outputDir, relativePath);
-  mkdirp.sync(path.dirname(outputFilePath));
-  fs.writeFileSync(outputFilePath, code);
+    const relativePath = path.relative(inputDir, fileName);
+    const outputFilePath = path.join(outputDir, relativePath);
+    mkdirp.sync(path.dirname(outputFilePath));
+    fs.writeFileSync(outputFilePath, code);
+  } catch (err) {
+    console.error('Error for file:', fileName);
+    console.error(err);
+  }
 };
 
 // If running this script for the second time, it should not
