556db8398579f0f5fcc26c5318bb0a2e5915cebd
Souradeep Nanda <souradeep.nanda123@gmail.com>
2019-05-14 19:54:16 +0530

    Added more aspirations

---
+++ test/fixtures/[todo]alerts/output.js
@@ -0,0 +1,11 @@
+// TODO: Maybe not. Very few production codes have alerts
+// Not worth the effort
+const someFun = () => {
+  alert('Regular text');
+  alert(`multiline
+  text`);
+  const someVal = 'potatoes';
+  alert(`I like ${someVal}`);
+};
+
+export default someFun;
