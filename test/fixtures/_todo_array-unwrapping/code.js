344b699b75a2fee66adac1a1819822d80ccbfd08
Souradeep Nanda <souradeep.nanda123@gmail.com>
2019-05-17 21:34:49 +0530

    Aspirational test for arr of obj

--- test/fixtures/_todo_array-unwrapping/code.js
+++ test/fixtures/_todo_array-unwrapping/code.js
@@ -3,11 +3,27 @@
 import React from 'react';
 
 const vegetables = ['potato', 'tomato', 'cabbage'];
+const fridge = [
+  {
+    name: 'potato',
+    description: 'large potato',
+  },
+  {
+    name: 'tomato',
+    description: 'large tomato',
+  },
+];
 
 const MyComponent = () => (
   <ul>
     {vegetables.map(vegetable => (
       <li>{vegetable}</li>
+    ))}
+    {fridge.map(item => (
+      <div>
+        <div>{item.name}</div>
+        <div>{item.description}</div>
+      </div>
     ))}
   </ul>
 );
--- test/fixtures/_todo_array-unwrapping/output.js
+++ test/fixtures/_todo_array-unwrapping/output.js
@@ -1,13 +1,29 @@
-// TODO: Find a way to implement this feature
+import i18n from 'i18next';import k from '~/i18n/keys'; // TODO: Find a way to implement this feature
 
 import React from 'react';
 
 const vegetables = ['potato', 'tomato', 'cabbage'];
+const fridge = [
+{
+  name: 'potato',
+  description: 'large potato' },
+
+{
+  name: i18n.t(k.TOMATO1),
+  description: i18n.t(k.LARGE_TOMATO) }];
+
+
 
 const MyComponent = () =>
 <ul>
     {vegetables.map((vegetable) =>
   <li>{vegetable}</li>)}
+
+    {fridge.map((item) =>
+  <div>
+        <div>{item.name}</div>
+        <div>{item.description}</div>
+      </div>)}
 
   </ul>;
 
